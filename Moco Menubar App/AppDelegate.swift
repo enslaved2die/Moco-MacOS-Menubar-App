767a00bcf65e38c098f0d337eedef94d33a57e80
Joshua <sellke.joshua@gmail.com>
2025-03-25 12:06:45 +0100

    improved blinking now more random

--- Moco Menubar App/AppDelegate.swift
+++ Moco Menubar App/AppDelegate.swift
@@ -3,12 +3,14 @@ import SwiftUI
 import AppKit
 
 // MARK: - AppDelegate
-class AppDelegate: NSObject, NSApplicationDelegate, BookingDelegate { // 4. Implement the delegate
-    var statusItem: NSStatusItem!
+class AppDelegate: NSObject, NSApplicationDelegate, BookingDelegate {
+    var statusItem: NSStatusItem?
     var popover: NSPopover!
     var isIconVisible: Bool = true
     var timer: Timer?
     var lastBookingDate: Date?
+    var blinkHold: Double = 0.5
+    var isBlinking = false
     
     let openIcon = NSImage(named: "MocoIconTray")
     let closedIcon = NSImage(named: "MocoIconTrayClosed")
@@ -18,84 +20,94 @@ class AppDelegate: NSObject, NSApplicationDelegate, BookingDelegate { // 4. Impl
         NSApp.setActivationPolicy(.prohibited)
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         
-        if let button = statusItem.button {
+        if let button = statusItem?.button {
             button.image = openIcon
             button.action = #selector(togglePopover)
+            button.target = self
             
         }
         
         popover = NSPopover()
         popover.contentSize = NSSize(width: 300, height: 350)
-        // Use the existing BookingView
-        var bookingView = BookingView() // Changed to var
-        // 5. Set the delegate
+        var bookingView = BookingView()
         bookingView.delegate = self
         popover.contentViewController = NSViewController()
         popover.contentViewController?.view = NSHostingView(rootView: bookingView)
         popover.behavior = .transient
         
-        // Get the last booking date
         getLastBookingDate()
         print("AppDidFinishLaunching - Last Booking Date: \(String(describing: lastBookingDate))")
         
-        // Start the timer to check for inactivity
-        startTimer()
+        startTimer() // Start the timer to check for blinking
     }
     
-    @objc func togglePopover() {
+    @objc func togglePopover(_ sender: NSStatusBarButton) {
         if popover.isShown {
             popover.performClose(nil)
-        } else if let button = statusItem.button {
+        } else if let button = statusItem?.button {
             popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
         }
     }
     
     func startBlinking() {
-        if timer == nil { // Check if timer is already running
-            let randomInterval = Double.random(in: 3.0...10.0)
-            timer = Timer.scheduledTimer(withTimeInterval: randomInterval, repeats: true) { [weak self] _ in
-                guard let self = self, let button = self.statusItem.button else { return }
+        guard let button = statusItem?.button else { return }
+        
+        if !isBlinking {
+            isBlinking = true
+            print("startBlinking - Start Blinking")
+        }
+        
+        blinkHold = Double.random(in: 0.5...2.0)
+        print(blinkHold)
+        
+        DispatchQueue.main.async {
+            button.image = self.closedIcon
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                button.image = self.openIcon
                 
-                DispatchQueue.main.async {
-                    button.image = self.closedIcon
-                    
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        button.image = self.openIcon
+                //  Check the time *before* scheduling the next blink.
+                let timeInterval = Date().timeIntervalSince(self.lastBookingDate ?? Date.distantPast)
+                let minutesSinceLastBooking = timeInterval / 60
+                
+                if minutesSinceLastBooking >= 1 && self.isBlinking {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + self.blinkHold) {
+                        self.startBlinking() // Continue blinking if condition is still met
                     }
+                } else {
+                    self.stopBlinking() // Stop blinking if condition is not met
                 }
             }
-            print("startBlinking - Timer started")
-        } else {
-            print("startBlinking - Timer already running")
         }
     }
     
     func stopBlinking() {
-        timer?.invalidate()
-        timer = nil
-        if let button = statusItem.button {
+        if let button = statusItem?.button {
             button.image = openIcon
         }
-        print("stopBlinking - Timer stopped")
+        isBlinking = false
+        print("stopBlinking")
     }
     
     func startTimer() {
-        Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(checkLastBooking), userInfo: nil, repeats: true)
+        // Use a timer to periodically check the booking time and start/stop blinking
+        timer = Timer.scheduledTimer(timeInterval: 60.0, target: self, selector: #selector(checkLastBooking), userInfo: nil, repeats: true)
     }
     
     @objc func checkLastBooking() {
         guard let lastBookingDate = lastBookingDate else {
-            startBlinking()
+            startBlinking() // Start blinking if no booking date
             return
         }
         
         let timeInterval = Date().timeIntervalSince(lastBookingDate)
-        print("checkLastBooking - Time Interval: \(timeInterval)")
         let minutesSinceLastBooking = timeInterval / 60
-        print("checkLastBooking - Minutes Since Last Booking: \(minutesSinceLastBooking)")
         
         if minutesSinceLastBooking >= 120 {
-            startBlinking()
+            if !isBlinking { // Start blinking only if it's not already blinking
+                startBlinking()
+            }
+            //Do nothing, blinking is handled by startBlinking
         } else {
             stopBlinking()
         }
@@ -106,21 +118,20 @@ class AppDelegate: NSObject, NSApplicationDelegate, BookingDelegate { // 4. Impl
             self.lastBookingDate = savedDate
             print("getLastBookingDate - Retrieved Date: \(savedDate)")
         } else {
-            self.lastBookingDate = nil;
+            self.lastBookingDate = nil
             print("getLastBookingDate - No Date Found")
         }
+    }
+    
+    func didUpdateLastBookingDate(date: Date) {
+        print("AppDelegate - Delegate method called with date: \(date)")
+        self.lastBookingDate = date
+        stopBlinking() // Stop blinking when a new booking is made
     }
     
     deinit {
         timer?.invalidate()
         print("AppDelegate - deinit")
-    }
-    
-    // 6. Implement the delegate method
-    func didUpdateLastBookingDate(date: Date) {
-        print("AppDelegate - Delegate method called with date: \(date)")
-        self.lastBookingDate = date
-        stopBlinking() // Stop blinking when a new booking is made
     }
 }
 
